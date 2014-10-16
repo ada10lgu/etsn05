88e3cae89d1d8b2e64b3a7594c5f4df3c3d49fdc
ronnaker <dat11jro@student.lu.se>
2014-10-16 10:50:23 +0200

    Added menus.

--- BaseBlockSystem/src/Administration.java
+++ BaseBlockSystem/src/Administration.java
@@ -194,7 +194,7 @@ public class Administration extends servletBase {
 		access.updateLog(null, null);
 		PrintWriter out = response.getWriter();
 		out.println(getPageIntro());
-
+		out.println(printMainMenu());
 		String myName = "";
 		HttpSession session = request.getSession(true);
 		Object nameObj = session.getAttribute("name");
