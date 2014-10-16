e893f37f0163cb90cd0adc2438b1aaf8c84d21d7
Henrik Nilsson <h_nilsson_@hotmail.com>
2014-10-16 12:52:10 +0200

    viewReport är färdig. Om null skickas till metoden så genereras en rapport med testvärden.

--- BaseBlockSystem/src/services/ReportGenerator.java
+++ BaseBlockSystem/src/services/ReportGenerator.java
@@ -8,24 +8,57 @@ import javax.servlet.http.HttpSession;
 
 public final class ReportGenerator {
 
+	private static String name = null;
+	private static Date date = null;
+	private static String project_group = null;
+	private static int week_number = -1;
+	private static int total_time = -1;
+	private static int signed = -1;
+	private static int[] act_sub = new int[45];
+	private static int[] act = new int[9];
+	private static int[] sub = new int[4];
+	
+	private static final int[] upper_numbers = new int[]{11, 12, 13, 14, 15, 16, 17, 18, 19};
+	private static final String[] upper_activities = new String[]{"SDP", "SRS", "SVVS", "STLDD", "SVVI", "SDDD", "SVVR", "SSD", "Slutrapport"};
+	private static final int[] lower_numbers = new int[]{21, 22, 23, 30, 41, 42, 43, 44, 100};
+	private static final String[] lower_activities = new String[]{"Funktionstest", "Systemtest", "Regressionstest", "Meeting", "Lecture", "Excersice", "Terminal", "Study", "Other"};
+	private static final String[] activity_type = new String[]{"Utveckling och dokumentation", "Informell granskning", "Formell granskning", "Ombearbetning"};
+	private static final String[] activity_code = new String[]{"U", "I", "F", "O"};
+	private static final String[] activity_description = new String[]{"Utveckla ny kod, testfall och dokumentation inklusive dokumentation av systemet",
+		"Tid spenderad på förberedelser inför och på informella granskningar", "Tid spenderad på förberedelser inför och på formella granskningar",
+		"Tid spenderad på ombearbetning, förbättring, revision eller korrektion av dokument och design objekt"};
+	
 	private ReportGenerator() {
 		
 	}
 	
-	/**
-	 * Generates a String-representation of a time report containing the data specified by the data parameter.
-	 * @param data: Contains the data that should be printed in the time report.
-	 */
-	public static String viewReport(ResultSet data) {
+	private static void init_test_data() {
+		name = "Test User";
+		date = new Date(0);
+		project_group = "Test Group";
+		week_number = 1;
+		total_time = 100;
+		signed = 1;
+		for (int i = 0; i < act_sub.length; i++) {
+			act_sub[i] = i;
+		}
+		for (int i = 0; i < act.length; i++) {
+			act[i] = i;
+		}
+		for (int i = 0; i < sub.length; i++) {
+			sub[i] = i;
+		}
+	}
+	
+	private static void init_data(ResultSet data) {
 		try {
-			String name = data.getString("username");
-			Date date = data.getDate("date");
-			String project_group = data.getString("name");
-			int week_number = data.getInt("week");
-			int total_time = data.getInt("total_time");
-			int signed = data.getInt("signed");
+			name = data.getString("username");
+			date = data.getDate("date");
+			project_group = data.getString("name");
+			week_number = data.getInt("week");
+			total_time = data.getInt("total_time");
+			signed = data.getInt("signed");
 			
-			int[] act_sub = new int[45];
 			act_sub[0] = data.getInt("SDP_U");
 			act_sub[1] = data.getInt("SDP_I");
 			act_sub[2] = data.getInt("SDP_F");
@@ -72,7 +105,6 @@ public final class ReportGenerator {
 			act_sub[43] = data.getInt("Slutrapport_O");
 			act_sub[44] = act_sub[40] + act_sub[41] +act_sub[42] +act_sub[43];
 			
-			int[] act = new int[9];
 			act[0] = data.getInt("Funktionstest");
 			act[1] = data.getInt("Systemtest");
 			act[2] = data.getInt("Regressionstest");
@@ -83,7 +115,6 @@ public final class ReportGenerator {
 			act[7] = data.getInt("Study");
 			act[8] = data.getInt("Other");
 			
-			int[] sub = new int[4];
 			sub[0] = act_sub[0] + act_sub[5] +act_sub[10] +act_sub[15] + act_sub[20] + act_sub[25] +act_sub[30] +act_sub[35] + act_sub[40];
 			sub[1] = act_sub[1] + act_sub[6] +act_sub[11] +act_sub[16] + act_sub[21] + act_sub[26] +act_sub[31] +act_sub[36] + act_sub[41];
 			sub[3] = act_sub[2] + act_sub[7] +act_sub[12] +act_sub[17] + act_sub[22] + act_sub[27] +act_sub[32] +act_sub[37] + act_sub[42];
@@ -91,14 +122,117 @@ public final class ReportGenerator {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Generates a String-representation of a time report containing the data specified by the data parameter.
+	 * @param data: Contains the data that should be printed in the time report.
+	 */
+	public static String viewReport(ResultSet data) {
+		if (data == null) {
+			init_test_data();
+		} else {
+			init_data(data);
+		}
+		
 		String html = "";
-		
-		html += "<form>";
+		html += "<style type='text/css'>"
+				+ "td {border: 1px solid black; width: 100px;}"
+				+ ".grey {background-color: #dddddd;}"
+				+ "</style>";
+		html += "<table style='border: 1px solid black; border-collapse: collapse; width: 700px; text-align: left;'>";
 		html += 	"<tr>";
+		html +=			"<td><b>Namn</td><td colspan='4'>"+name+"</td><td><b>Datum</td><td>"+date.toString()+"</td>";
 		html += 	"</tr>";		
-		html += "</form>";
+		html += 	"<tr>";
+		html +=			"<td><b>Projektgrupp</td><td colspan='4'>"+project_group+"</td><td><b>Vecka</td><td>"+week_number+"</td>";
+		html += 	"</tr>";
+		html += 	"<tr>";
+		html += 		"<td colspan='6' class='grey'><b>Del A - Total tid denna vecka (minuter)</td><td>"+total_time+"</td>";
+		html += 	"</tr>";
+		html += 	"<tr>";
+		html += 		"<td colspan='7' class='grey'><b>Del B - Antal minuter per aktivitet och subaktivitet</td>";
+		html += 	"</tr>";
+		html += 	"<tr>";
+		html += 		"<td><b>Nummer</td><td><b>Aktivitet</td><td><b>U</td><td><b>I</td><td><b>F</td><td><b>O</td><td><b>Totaltid</td>";
+		html += 	"</tr>";
+		int k = 0;
+		for (int i = 0; i < 9; i++) {
+			html += "<tr>";
+			for (int j = 0; j < 7; j++) {
+				if (j == 0) {
+					html += "<td>"+upper_numbers[i]+"</td>";
+				}
+				if (j == 1) {
+					html += "<td>"+upper_activities[i]+"</td>";
+				}
+				if (j > 1) {
+					html += "<td>"+act_sub[k]+"</td>";
+					k++;
+				}	
+			}
+			html += "</tr>";
+		}
+		html +=		"<tr>";
+		html += 		"<td><b>Summa</td><td class='grey'></td><td>"+sub[0]+"</td><td>"+sub[1]+"</td><td>"+sub[2]+"</td><td>"+sub[3]+"</td><td class='grey'></td>";			
+		html +=		"</tr>";
+		k = 0;
+		for (int i = 0; i < 9; i++) {
+			html += "<tr>";
+			for (int j = 0; j < 3; j++) {
+				if (j == 0) {
+					html += "<td>"+lower_numbers[i]+"</td>";
+				}
+				if (j == 1) {
+					html += "<td colspan='5'>"+lower_activities[i]+"</td>";
+				}
+				if (j == 2) {
+					html += "<td>"+act_sub[k]+"</td>";
+					k++;
+				}	
+			}
+			html += "</tr>";
+		}
+		html += 	"<tr>";
+		html += 		"<td colspan='7' class='grey'><b>Del C - Antal minuter spenderat på de olika subaktiviteterna</td>";
+		html += 	"</tr>";
+		html += 	"<tr>";
+		html += 		"<td><b>Aktivitetstyp</td><td><b>Aktivitetskod</td><td colspan='4'><b>Beskrivning</td><td><b>Summa</td>";
+		html += 	"</tr>";
+		k = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (j == 0) {
+					html += "<td>"+activity_type[i]+"</td>";
+				}
+				if (j == 1) {
+					html += "<td>"+activity_code[i]+"</td>";
+				}
+				if (j == 2) {
+					html += "<td colspan='4'>"+activity_description[i]+"</td>";
+				}
+				if (j == 3) {
+					html += "<td>"+sub[i]+"</td>";
+				}
+			}
+			html += "</tr>";
+		}	
+		html += 	"<tr>";
+		html += 		"<td colspan='7' class='grey'><b>Del D - Signatur</td>";
+		html += 	"</tr>";
+		html += 	"<tr>";
+		html += 		"<td><b>Signerad</td><td colspan='5'></td>";
+		if (signed == 0) {
+			html +=		"<td>NO</td>";
+		}
+		if (signed == 1) {
+			html +=		"<td>YES</td>";
+		}
+		html += 	"</tr>";
 		
-		return null;
+		html += "</table>";
+		
+		return html;
 		
 	}
 	
