b1f682d223b36cb3d0508513af543bb265ce20ca
Axel <Axel_ulmestig@hotmail.com>
2014-10-18 16:24:22 +0200

    lagt till metod för inloggning i PussTest

--- Testing/src/test/AdministrationTest.java
+++ Testing/src/test/AdministrationTest.java
@@ -5,8 +5,5 @@ import java.sql.SQLException;
 import org.junit.Test;
 
 public class AdministrationTest extends PussTest{
-	@Test
-	public void FT2_5_4(){		
-		
-	}
+	
 }
--- Testing/src/test/PussTest.java
+++ Testing/src/test/PussTest.java
@@ -1,6 +1,9 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,10 +11,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
 public abstract class PussTest {
-	protected static Connection conn = null;	
+	protected static Connection conn = null;
+	protected static WebClient webClient = null;
 	
 	public static final String TOMCAT_PATH = "tomcat/apache-tomcat-7.0.55/bin/";
 	public static final String STARTUP_SHELL = "startup.sh";
@@ -36,33 +51,78 @@ public abstract class PussTest {
 	@AfterClass
 	public static void tearDown() {
 		shutDownServer();
+		try {
+			webClient.closeAllWindows();
+		} catch(Exception e) {
+//			e.printStackTrace();
+		}
 	}
 	
-	protected ResultSet sendSQLCommand(String query) throws SQLException {
+	protected void sendSQLCommand(String query) throws SQLException {
 		Statement stmt = conn.createStatement();
-		return stmt.executeQuery(query);		
+		stmt.executeUpdate(query);		
+	}
+	
+	protected ResultSet sendSQLQuery(String query) throws SQLException {
+		Statement stmt = conn.createStatement();
+		return stmt.executeQuery(query);
+	}
+	
+	protected void clearDataBase() {
+		//TODO implementera när det finns en test DB.
 	}
 	
 	protected int addUser(String username, String password, int is_admin) throws SQLException {
 		String query = "insert into users (username, password, is_admin) values ('" + username + "', '" + password + "', " + is_admin +");";
 		sendSQLCommand(query);
 		query = "select id from users where username='" + username + "';";
-		ResultSet rs = sendSQLCommand(query);
-		return rs.getInt(0);
+		ResultSet rs = sendSQLQuery(query);
+		rs.next();
+		return rs.getInt(1);
 	}
 	
 	protected void deleteUser(String username) throws SQLException {
-		String query = "delete from users where username  = '" + username + "';";
+		String query = "select id from users where username = '" + username + "';";
+		ResultSet rs = sendSQLQuery(query);
+		rs.next();
+		int userId = rs.getInt(1);
+		query = "delete from user_group where user_id = " + userId + ";";
 		sendSQLCommand(query);
+		
+		query = "delete from log where user_id = " + userId + ";";
+		sendSQLCommand(query);
+		
+		query = "delete from users where username  = '" + username + "';";
+		sendSQLCommand(query);
+		
 	}
 	
-	protected void addGroup(String name) throws SQLException {
-		String query = "insert into groups (name) values ('" + name + "');";
-		sendSQLCommand(query);
+	protected void deleteGroup(String groupName) throws SQLException {
+		String query = "select id from groups where name = '" + groupName + "';";
+		ResultSet rs = sendSQLQuery(query);
+		if(rs.next()) {
+			int groupId = rs.getInt(1);
+			
+			query = "delete from user_group where group_id = " + groupId + ";";
+			sendSQLCommand(query);
+			
+			query = "delete from groups where name = '" + groupName + "';";
+			sendSQLCommand(query);
+		}			
 	}
 	
-	protected void assignGroup(String username, String group) throws SQLException {
-		String query = "";
+	protected int addGroup(String groupName) throws SQLException {
+		String query = "insert into groups (name) values ('" + groupName + "');";
+		sendSQLCommand(query);
+		query = "select id from groups where name ='" + groupName + "';";
+		ResultSet rs = sendSQLQuery(query);
+		rs.next();
+		return rs.getInt(1);
+	}
+	
+	protected void assignGroup(int userId, int groupId, String role) throws SQLException {
+		String query = "insert into user_group (user_id, group_id, role) values (" + userId + ", " + groupId + ", '" + role + "');";
+		sendSQLCommand(query);
 	}
 	
 	protected static void startServer() {
@@ -72,6 +132,38 @@ public abstract class PussTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	protected HtmlPage login(String username, String password, String group) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		webClient = new WebClient();
+
+	    // Get the first page
+	    final HtmlPage page1 = webClient.getPage(LOGIN_URL);
+
+	    // Get the form that we are dealing with and within that form, 
+	    // find the submit button and the field that we want to change.
+	    final HtmlForm form = page1.getFormByName("input");
+
+	    final HtmlSubmitInput button = form.getInputByValue("Submit");
+	    final HtmlTextInput userField = form.getInputByName("user");
+	    final HtmlPasswordInput passwordField = form.getInputByName("password");
+	    final HtmlSelect groupList = form.getSelectByName("groupID");
+
+	    // Change the value of the text field
+	    userField.setValueAttribute(username);
+	    passwordField.setValueAttribute(password);
+	    groupList.setSelectedAttribute(groupList.getOptionByText(group), true);
+	    //	    groupList.setSelectedAttribute(LOGIN_T3, true);
+
+	    // Now submit the form by clicking the button and get back the second page.
+	    final HtmlPage page2 = button.click();
+
+//	    assertEquals("jonatan could not log in", START_URL, page2.getUrl().toString());
+
+//	    HtmlAnchor logout = page2.getAnchorByHref("LogIn");
+//	    logout.click();
+//	    webClient.closeAllWindows();
+	    return page2;
 	}
 	
 	protected static void shutDownServer() {
